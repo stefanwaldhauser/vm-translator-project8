111fc817aa127a9a209bfa9927cf4d7be1dd9ecd
Stefan Waldhauser <stefan.waldhauser@proton.me>
2024-11-01 12:34:05 +0100

    Added Directory Handling

--- VMTranslator.py
+++ VMTranslator.py
@@ -40,26 +40,37 @@ def translate_vm_file(file_path):
                 yield translate_line(translator, line)
 
 
-def translate_file(input_path):
-    try:
-        vm_path, asm_path = get_paths(input_path)
-        asm_commands = translate_vm_file(vm_path)
+def translate_single_file(path):
+    asm_commands = translate_vm_file(path)
+    with open(path.with_suffix('.asm'), 'w', encoding='UTF-8') as f:
+        # ToDo: First write init code
+        for command_list in asm_commands:
+            f.write('\n'.join(command_list) + '\n')
 
-        with open(asm_path, 'w', encoding='UTF-8') as f:
+
+def translate_directory(path):
+    dir_name = path.stem
+    with open(path.joinpath(f"{dir_name}.asm"), 'w', encoding='UTF-8') as f:
+        # ToDo: First write init code
+        for file_path in path.glob('*.vm'):
+            f.write(f"//Translating {file_path.stem}" + '\n')
+            asm_commands = translate_vm_file(file_path)
             for command_list in asm_commands:
                 f.write('\n'.join(command_list) + '\n')
 
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python VMTranslator.py <filename.vm>")
-        sys.exit(1)
-
-    translate_file(sys.argv[1])
+    # No path specified -> translator operates on current folder
+    if len(sys.argv) == 1:
+        path = Path(os.getcwd())
+    # Path to a directory <Directory> specified -> Translate all .vm files inside of that directorty into a single <Directory>.asm file and place it in the directory
+    else:
+        path = Path(sys.argv[1])
+     # Path to one <FileName>.vm file specified -> Translate to <FileName>.asm and place it next to the source
+    if path.is_file():
+        translate_single_file(path)
+    else:
+        translate_directory(path)
 
 
 if __name__ == "__main__":
